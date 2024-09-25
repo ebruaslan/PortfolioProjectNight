b30b27bb4388a6bfcf915b09c3ae8b92a7420611
Ebru ASLAN <ebruaslan.1453@gmail.com>
2024-09-25 21:18:23 +0300

    Check

---
+++ PortfolioProjectNight/Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using PortfolioProjectNight.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNight.Controllers
+{
+    public class SkillController : Controller
+    {
+        // GET: Skill
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+    }
+}
